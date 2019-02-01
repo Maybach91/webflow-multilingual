4c8e6d4a187a07c75cbac318f131d2f9b28e5d57
Patrick Ludewig <yes@patrick.works>
2019-02-01 12:51:15 +0100

    dont change urls on homepage because of the missing slash at the end

--- language-dropdown.js
+++ language-dropdown.js
@@ -6,50 +6,61 @@ const addUrls = (function() {
     const links = dropdown.querySelectorAll(".dropdown-link");
     const origin = window.location.origin; // https://www.example.com
     let path = window.location.pathname; // /post/blogpost-name-lorem
+    let fullUrl;
 
-    // go through every link and get the language code from the language dropdown
-    Array.prototype.forEach.call(links, function(el, i) {
-      let attr = el.getAttribute("href");
-      if (attr === "/") {
-        attr = "";
-      }
-      const regexLang = /\/([\w]{2})\//g;
-      const arrPath = regexLang.exec(path);
-      if (arrPath) {
-        path = path.replace(regexLang, "");
-      }
+    // If Homepage dont do anything otherwise change links
+    if (path === "/" || path === "/de" || path === "/fr") {
+      return false;
+    } else {
+      // go through every link and get the language code from the language dropdown
+      Array.prototype.forEach.call(links, function(el, i) {
+        let attr = el.getAttribute("href");
 
-      const fullUrl = origin + attr + path;
-
-      // Check if the requested site exists in the language and set it to e.g  /en/delivery otherwise do nothing and redirect to /en
-      const request = new XMLHttpRequest();
-      request.open("GET", fullUrl, true);
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-          // Site is translated, to update the links
-
-          let newPath = attr + path; // make a relative path out of it
-
-          el.setAttribute("href", newPath);
-        } else {
-          // do nothing because the site is not there in the language
+        if (attr === "/") {
+          attr = "";
         }
-      };
-      request.onerror = function() {
-        // do nothing because the site is not there in the language
-      };
-      request.send();
 
-      // If Blog / or Blog Category, then add get param to set the language (because all translated content is on one page - because of collections)
-      const arrParam =
-        /\/post\//g.exec(location.pathname) ||
-        /\/category\//g.exec(location.pathname);
+        const regexLang = /\/(:?[\w]{2})\//g; // should discover just the "/de/" or "/en/" or "/fr/" or any 2 characters between slashes, but only the first
+        const arrPath = regexLang.exec(path);
+        if (arrPath) {
+          path = path.replace(regexLang, "/");
+        }
+        const fullUrl = origin + attr + path; // e.g. https://www.example.com/de/example-site
+        // If Blog / or Blog Category, then add get param to set the language (because all translated content is on one page - because of collections)
+        const arrParam =
+          /magazin/g.exec(location.pathname) ||
+          /\/article|post|artikel\//g.exec(location.pathname) ||
+          /\/category|cat|kategorie\//g.exec(location.pathname);
 
-      if (arrParam) {
-        const langPath = attr.replace("/", ""); // cut the / from the default href of the link
-        const newPath = origin + path + "?lang=" + langPath; // make a absolute path out of it
-        el.setAttribute("href", newPath); // set the attribute
-      }
-    });
+        if (arrParam) {
+          const langPath = attr.replace("/", ""); // cut the / from the default href of the link
+          //const newPath = origin + path + "?lang=" + langPath; // make a absolute path out of it
+          const newPathMagazine = origin + attr + "/magazine"; // make a absolute path out of it
+          el.setAttribute("href", newPathMagazine); // set the attribute
+
+          if (attr === "/de") {
+            el.setAttribute("href", origin + attr + "/magazin"); // set the attribute
+          }
+        } else {
+          // Check if the requested site exists in the language and set it to e.g  /en/delivery otherwise do nothing and redirect to /en
+          const request = new XMLHttpRequest();
+          request.open("GET", fullUrl, true);
+          request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+              // Site is translated, to update the links
+
+              let newPath = attr + path; // make a relative path out of it
+              el.setAttribute("href", newPath);
+            } else {
+              // do nothing because the site is not there in the language
+            }
+          };
+          request.onerror = function() {
+            // do nothing because the site is not there in the language
+          };
+          request.send();
+        }
+      });
+    }
   }
 })();
